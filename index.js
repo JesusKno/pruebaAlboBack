aed2b7e7b8a94377b23bcca3ccca47a979d15b98
JesusKno <57113540+JesusKno@users.noreply.github.com>
2024-07-10 02:51:38 -0600

    se agrego e importaron bibliotecas para parsear el body a json

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import db from './config/db.js'
+import taskRouter from './routes/tasksRouter.js'
+import bodyParser from 'body-parser'
 
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 app.listen(3000)
+
+app.use('/', taskRouter)
 
 
 console.log("server on port", 3000)
