ccc00739c7c87cfa0dd377a179a88d39c67c4d8d
JesusKno <57113540+JesusKno@users.noreply.github.com>
2024-07-12 04:00:27 -0600

    se agrego la libreria cors para el manejo de los mismos

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import express from 'express'
 import db from './config/db.js'
 import taskRouter from './routes/tasksRouter.js'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors())
 
 
-app.listen(3000)
+app.listen(3001)
 
 app.use('/', taskRouter)
 
 
-console.log("server on port", 3000)
+console.log("server on port", 3001)
